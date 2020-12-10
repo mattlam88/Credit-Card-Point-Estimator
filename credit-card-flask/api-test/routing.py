8ab878c4ab2464ea7541795256163a9bcd678f67
mattlam88 <63694045+mattlam88@users.noreply.github.com>
2020-12-09 19:04:19 -0500

    Sorted files and folders

---
+++ credit-card-flask/api-test/routing.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/hello', methods=['GET'])
+def hello_world():
+    return {
+        'text': 'Hello, World!',
+        'status': 'completed'
+    }
