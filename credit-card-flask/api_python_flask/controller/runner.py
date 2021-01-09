dacc61955687c355c304a0b822ee2c96bce9fee3
mattlam88 <63694045+mattlam88@users.noreply.github.com>
2021-01-09 00:11:37 -0500

    refactored backend code

--- credit-card-flask/api_python_flask/controller/runner.py
+++ credit-card-flask/api_python_flask/controller/runner.py
@@ -5,6 +5,7 @@ from api_python_flask.model.yearly_budget_model import YearlyBudgetDAO, YearlyBu
 from api_python_flask.model.credit_card_model import CreditCardDAO, CreditCard
 from api_python_flask.model.reward_points_model import RewardPointsDAO, RewardPoints
 from api_python_flask.model.user_model import UserDAO, User
+from api_python_flask.model.user_max_multipliers import UserMaxCreditCardMult, UserMaxCreditCardMultDAO
 from api_python_flask.model.reward_points_expected_value_model import RewardPointsEVDAO, RewardPointsEV
 
 class MonthlyBudgetService:
@@ -13,10 +14,9 @@ class MonthlyBudgetService:
 
     def calculate_total_monthly_budget(self, username, month, year):
         monthly_spend = MonthlyBudgetDAO.get_monthly_budget(username, month, year)
-        total_month_spend = monthly_spend[4]+monthly_spend[5]+monthly_spend[6]+monthly_spend[7]+monthly_spend[8]
+        total_month_spend = monthly_spend.restaurant_spend + monthly_spend.grocery_spend + monthly_spend.non_cat_spend + monthly_spend.utility_spend + monthly_spend.gas_spend
         self.MonthlyBudgetDAO.edit_monthly_total_spend(username, month, year, total_month_spend)
         # this will sum up each month by adding up each category spend
-
 
 class YearlyBudgetService:
     def __init__(self):
@@ -28,38 +28,41 @@ class YearlyBudgetService:
         self.YearlyBudgetDAO.edit_total_yearly_category_spend(username, year, total_yearly_category_spend)
 
     def calc_total_yearly_spend(self, username, year):
-        total_yearly_spend = MonthlyBudgetDAO.get_all_monthly_user_spend(username, year)
-        self.YearlyBudgetDAO.edit_yearly_budget(username, year, total_yearly_spend)
+        year_spend = MonthlyBudgetDAO.get_all_monthly_user_spend(username, year)
+        total_year_spend = sum(year_spend.jan_spend, year_spend.feb_spend, year_spend.mar_spend, year_spend.apr_spend, year_spend.may_spend, year_spend.june_spend, year_spend.july_spend, year_spend.aug_spend, year_spend.sept_spend, year_spend.oct_spend, year_spend.nov_spend, year_spend.dec_spend)
+        self.YearlyBudgetDAO.edit_yearly_budget(username, year, total_year_spend)
         # update the yearlySpend table with the total yearly spend by aggregating all of the monthly spend totals
 
 class RewardPointsService:
     def __init__(self):
-        self.YearlyBudgetDAO = YearlyBudgetDAO()
-        self.CreditCardDAO = CreditCardDAO()
+        self.UserMaxCreditCardMultDAO = UserMaxCreditCardMultDAO()
+        self.UserMaxCreditCardMult = UserMaxCreditCardMult()
         self.UserDAO = UserDAO()
         self.User = User()
+        self.MonthlyBudgetDAO = MonthlyBudgetDAO()
+        self.MonthlyBudget = MonthlyBudget()
         self.RewardPointsDAO = RewardPointsDAO()
+        self.RewardPoints = RewardPoints()
 
     def calc_reward_points_monthly(self, username, month, year):
-        users_multipliers = UserDAO.get_max_multipliers(username)
+        users_multipliers = UserMaxCreditCardMultDAO.get_max_multipliers(username)
         month_spend = MonthlyBudgetDAO.get_monthly_budget(username, month, year)
 
-        restaraunt_points = users_multipliers[0]*month_spend[4]
-        grocery_points = users_multipliers[1]*month_spend[5]
-        non_category_points = users_multipliers[2]*month_spend[6]
-        utility_points = users_multipliers[3]*month_spend[7]
-        gas_points =users_multipliers[4]*month_spend[8]
+        restaraunt_points = users_multipliers.restaurant_mult*month_spend.restaurant_spend
+        grocery_points = users_multipliers.grocery_mult*month_spend.grocery_spend
+        non_category_points = users_multipliers.non_cat_mult*month_spend.non_cat_spend
+        utility_points = users_multipliers.utility_mult*month_spend.utility_spend
+        gas_points =users_multipliers.gas_mult*month_spend.gas_spend
         total_monthly_points = sum(restaraunt_points, grocery_points, non_category_points, utility_points, gas_points)
 
         self.RewardPointsDAO.update_monthly_reward_points(username, month, year, total_monthly_points) 
         # This pulls the max multipliers from the user table and spend from the budgets table
         # The algo then multiplies each category by the respective multiplier, which then updates the rewards point table 
 
-    def calc_reward_points_yearly(self,username, year):
+    def calc_reward_points_yearly(self,username, year): #WIP THIS NEEDS TO BE UPDATED AND EVERYTHING BELOW THIS
         list_monthly_points = self.RewardPointsDAO.get_all_monthly_reward_points(username, year)
         total_year_points = sum(list_monthly_points)
         self.RewardPointsDAO.update_total_year_points(username, year, total_year_points)
-
 
 class FMVRewardsPointsService:
     def __init__(self):
--- credit-card-flask/api_python_flask/model/monthly_budget_model.py
+++ credit-card-flask/api_python_flask/model/monthly_budget_model.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+
 class MonthlyBudgetDAO:
     def __init__(self):
         self.conn = sqlite3.connect("creditCard.db")
@@ -18,15 +19,17 @@ class MonthlyBudgetDAO:
         total_monthly_category_spend = self.cur.execute(
             f"SELECT username, year, sum(dollars) restaurantSpend, sum(dollars) grocerySpend, sum(dollars) nonCategorySpend, sum(dollars) utilitySpend, sum(dollars) gasSpend FROM monthlyBudget WHERE username = {username} AND year = {year};")
         for spend in total_monthly_category_spend:
-            monthly_category_spend = YearlyCategorySpend(spend[0], spend[1], spend[2], spend[3], spend[4], spend[5], spend[6])
+            monthly_category_spend = YearlyCategorySpend(
+                spend[0], spend[1], spend[2], spend[3], spend[4], spend[5], spend[6])
         return monthly_category_spend
-        
+
     def get_all_monthly_user_spend(self, username, year):
-        cursor = self.cur.execute(
-            f"SELECT sum(dollars) monthlySpend FROM monthlyBudget WHERE username={username} AND year={year};")
-        for row in cursor:
-            year_spend = row[0]
-        return year_spend
+        months=["jan", "feb", "mar", "apr", "may", "june", "july", "aug", "sept", "oct", "nov", "dec"]
+        for month in months:
+            self.cur.execute(f"SELECT sum(dollars) monthlySpend FROM monthlyBudget WHERE username={username} AND year={year} AND month={month};")
+            monthly_spend_YTD = MonthlyYTDSpend(username, year, month[0], month[1], month[2], month[3], month[4], month[5], month[6], month[7], month[8], month[9], month[10], month[11])
+        return monthly_spend_YTD
+        # Will need to think through this
 
     def add_monthly_budget(self, data):
         self.cur.execute(
@@ -63,11 +66,28 @@ class MonthlyBudget:
         self.gas_spend = gas_spend
         self.monthly_spend = monthly_spend
 
-class MonthlySpend:
-    def __init__(self, username=None, month=None, year=0, total_month_spend=0):
+
+class MonthlyYTDSpend:
+    def __init__(self, username=None, year=0, jan_spend=0, feb_spend=0,
+                 mar_spend=0, apr_spend=0, may_spend=0, june_spend=0,
+                 july_spend=0, aug_spend=0, sept_spend=0, oct_spend=0,
+                 nov_spend=0, dec_spend=0):
         self.username = username
-        self.jan_spend
+        self.year = year
+        self.jan_spend = jan_spend
+        self.feb_spend = feb_spend
+        self.mar_spend = mar_spend
+        self.apr_spend = apr_spend
+        self.may_spend = may_spend
+        self.june_spend = june_spend
+        self.july_spend = july_spend
+        self.aug_spend = aug_spend
+        self.sept_spend = sept_spend
+        self.oct_spend = oct_spend
+        self.nov_spend = nov_spend
+        self.dec_spend = dec_spend
         # will need to add the following months
+
 
 class YearlyCategorySpend:
     def __init__(self, username=None, year=0, yearly_restaurant_spend=0, yearly_grocery_spend=0, yearly_non_cat_spend=0, yearly_utility_spend=0, yearly_gas_spend=0):
@@ -78,5 +98,3 @@ class YearlyCategorySpend:
         self.yearly_non_cat_spend = yearly_non_cat_spend
         self.yearly_utility_spend = yearly_utility_spend
         self.yearly_gas_spend = yearly_gas_spend
-
-
